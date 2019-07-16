49a671177755eeae579cedd85fd6001f06b544b6
Karthik N P <52942938+gh05t-4@users.noreply.github.com>
2019-07-16 07:31:22 +0530

    Add files via upload
    Do not use space while encrypting the pain text, but if you do it will be decrypted as 'N'.

---
+++ vigenere_v2.py
@@ -0,0 +1,92 @@
+
+"""
+
+Created on Mon Jul 15 11:16:02 2019
+
+ 
+
+@author: Sucheth
+
+"""
+
+ 
+
+count=0
+
+j=0
+
+temp_key=""
+
+key=""
+
+print("1.Encryption")
+
+print("2.Decryption")
+
+i=input("Choose an option(1 or 2 ): ")
+
+if(i=="1"):
+
+    msg=input("Enter Message To Encrypt: ")
+    message = msg.lower()
+    key=input("Key: ")
+
+    message_length=len(message)
+
+    key_length=len(key)
+    j=0
+    for count in range(0,message_length):
+        if(j==key_length):
+            j=0
+
+        temp_key+=key[j]
+        j+=1
+   
+
+    count=0
+
+    encrypted_message=""
+
+
+    while(count<message_length):
+
+        encrypted_message+=chr(((ord(message[count])+ord(temp_key[count]))%26+ord('A')))
+
+        count+=1
+
+    print("Encryped Message:\n",encrypted_message)    
+
+elif(i=="2"):
+
+    message=input("Enter the Message to Decrypt: ")
+
+    key=input("Enter Key: ")
+
+    message_length=len(message)
+
+    key_length=len(key)
+
+    count=0
+    j=0
+
+    for count in range(0,message_length):
+
+        if(j==key_length):
+            j=0
+
+        temp_key+=key[j]
+        j+=1
+    count=0
+
+    decrypted_message = ""
+
+    while(count<message_length):
+        decrypted_message+=chr(((ord(message[count])-ord(temp_key[count])+98)%26)+ord('A'))
+
+        count+=1
+
+    print("Decrypted Message:\n",decrypted_message)
+
+else:
+
+    print("Invalid Option")
